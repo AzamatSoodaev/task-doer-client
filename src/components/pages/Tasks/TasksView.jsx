7df6e5de516c3e579bd2b2036c4878222f83e87e
Azamat Soodaev <dreamy.kg33@gmail.com>
2021-03-25 18:33:45 +0600

    added tasks view page

---
+++ src/components/pages/Tasks/TasksView.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import "./styles.css";
+import AddTaskForm from "./AddTaskForm";
+import Tasks from "./Tasks";
+import TaskService from "../../../services/task.service";
+
+class TasksView extends React.Component {
+  state = {
+    currentTask: "",
+    userId: null,
+    tasks: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.fetchAllTasks();
+  }
+
+  fetchAllTasks() {
+    const { username } = this.props.match.params;
+    TaskService.fetchByUsername(username).then((response) => {
+      this.setState({
+        userId: response.data.id,
+        tasks: response.data.todos,
+      });
+    });
+  }
+
+  addTask = (event) => {
+    event.preventDefault();
+
+    if (this.state.currentTask === "") return;
+    this.setState({ loading: true });
+
+    TaskService.create({
+      todo: this.state.currentTask,
+      userId: this.state.userId,
+    }).then((response) => {
+      this.setState({
+        currentTask: "",
+        loading: false,
+      });
+      this.fetchAllTasks();
+    });
+  };
+
+  deleteTask = (id) => {
+    TaskService.deleteById(id).then((response) => {
+      this.fetchAllTasks();
+    });
+  };
+
+  completeTask = (task) => {
+    TaskService.updateById(task.id, {
+      completed: !task.completed,
+    }).then((res) => {
+      this.fetchAllTasks();
+    });
+  };
+
+  markAsImportant = (task) => {
+    TaskService.updateById(task.id, {
+      isImportant: !task.isImportant,
+    }).then((res) => {
+      this.fetchAllTasks();
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ currentTask: event.target.value });
+  };
+
+  editTask = (id, updatedTask) => {
+    TaskService.updateById(id, {
+      todo: updatedTask,
+    }).then((res) => {
+      this.fetchAllTasks();
+    });
+  };
+
+  render() {
+    return (
+      <div className="app-container">
+        <AddTaskForm
+          onAddTask={this.addTask}
+          onChange={this.handleInputChange}
+          setCurrentTask={this.state.currentTask}
+          loading={this.state.loading}
+        />
+
+        {this.state.tasks.length > 0 ? (
+          <Tasks
+            tasks={this.state.tasks}
+            onDeleteTask={this.deleteTask}
+            onChange={this.handleInputChange}
+            onCompleteTask={this.completeTask}
+            onMarkAsImportant={this.markAsImportant}
+            onEditTask={this.editTask}
+          />
+        ) : (
+          <p className="text-muted text-center">No Tasks</p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default TasksView;
