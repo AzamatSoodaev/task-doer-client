b91ef2753b840e051d36da74caf3b9665ebcb30c
Azamat Soodaev <dreamy.kg33@gmail.com>
2021-03-26 17:21:49 +0600

    code refactoring

--- src/components/pages/Tasks/Tasks.jsx
+++ src/components/pages/Tasks/Tasks.jsx
@@ -4,14 +4,7 @@ const Tasks = (props) => {
   return (
     <ul className="list-group list-group-flush">
       {props.tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          onDeleteTask={props.onDeleteTask}
-          onCompleteTask={props.onCompleteTask}
-          onMarkAsImportant={props.onMarkAsImportant}
-          onEditTask={props.onEditTask}
-        />
+        <Task key={task.id} task={task} {...props} />
       ))}
     </ul>
   );
